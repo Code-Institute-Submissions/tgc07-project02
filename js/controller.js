0cce251f30e73dc9434377cf2515c141d09af993
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-07-21 17:01:02 +0800

    Test search info is retrieved correctly

--- js/controller.js
+++ js/controller.js
@@ -12,6 +12,20 @@ window.addEventListener("load", (event) => {
     // console.log(event);
     // console.log(this);
     document.querySelector("#search-btn").addEventListener("click", () => {
-        alert("test search")
+        let searchCategory = getSearchCategory();
+        let searchInput = getSearchInput();
+        if (searchCategory==="business-name") {
+            console.log("business name selected");
+            console.log(searchInput);
+            // getBizByName(searchInput);
+        } else if (searchCategory==="address") {
+            console.log("address selected");
+            console.log(searchInput);
+            // getBizByAddress(searchInput);
+        } else if (searchCategory==="postcode") {
+            console.log("postcode selected");
+            console.log(searchInput);
+            // getBizByPostCode(searchInput);
+        };
     });
 });
