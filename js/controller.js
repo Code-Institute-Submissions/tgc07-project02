fad0ebdd383e492a3d4e66349808321689c4f771
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-07-23 13:04:21 +0800

    Remove old code

--- js/controller.js
+++ js/controller.js
@@ -24,20 +24,6 @@ window.addEventListener("load", (event) => {
     // console.log(event);
     // console.log(this);
     document.querySelector("#search-btn").addEventListener("click", () => {
-        // let searchCategory = getSearchCategory();
-        // let searchInput = getSearchInput();
-        // let searchHygieneRating = getHygieneRatingUserSelect();
-        // let searchResults = [];
-        // if (searchCategory==="business-name") {
-        //     searchResults = getBizByName(searchInput, searchHygieneRating);
-        //     console.log(searchResults);
-        // } else if (searchCategory==="address") {
-        //     searchResults = getBizByAddress(searchInput, searchHygieneRating);
-        //     console.log(searchResults);
-        // } else if (searchCategory==="postcode") {
-        //     searchResults = getBizByPostCode(searchInput, searchHygieneRating);
-        //     console.log(searchResults);
-        // };
         let bizName = getInputBizName();
         let address = getInputAddress();
         let postcode = getInputPostcode();
